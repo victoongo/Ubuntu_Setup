88c7f0d2b3d0774d675c7325293b888ad0f83681
Victor Wang <victoongo@gmail.com>
2015-05-06 15:11:31 -0400

    update to latest

--- git_filter_branch.sh
+++ git_filter_branch.sh
@@ -1,12 +1,12 @@
 # split a subdirectory of a larger repository into a new repository.
 
 # We assume a repository foo that contains a folder foobar that shall be a new repository called bar.
-git clone -no-hardlinks foo bar
+git clone --no-hardlinks foo bar
 cd bar
 git filter-branch --subdirectory-filter foobar HEAD --all
 git reflog expire --expire=now --all
-git reset -hard
-git gc -aggressive
+git reset --hard
+git gc --aggressive
 git prune
 
 # delete the old remote origin and set a new one.
@@ -15,6 +15,8 @@ git remote rm origin
 git pull git@hithub.com:user/bar.git
 git remote add origin git@hithub.com:user/bar.git
 git push origin master
+
+# cp ../foo/foobar/* . # copy other not tracked files over.
 
 # clean up the original repository.
 cd foo
--- tiny_VM_setup_pub.sh
+++ tiny_VM_setup_pub.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # A shell script to set up a minimalist Ubuntu VM based on 14.04 LTS Trusty Tahr mini.iso
-# for data science using R, python (Annaconda), PostgreSQL. 
-# I chose LightDM (with GTK+ Greeter), dwm, dmenu as my desktop environment. 
+# for data science using R, python (Annaconda), PostgreSQL.
+# I chose LightDM (with GTK+ Greeter), dwm, dmenu as my desktop environment.
 
 *** lightdm with gtk greeter
-sudo apt-get install --no-install-recommends xorg lightdm dwm dmenu make gcc 
+sudo apt-get install --no-install-recommends xorg lightdm dwm dmenu make gcc
 # xorg is needed for guestadditions to work well, xinit was not enough
-# sublime text needs gtk2 which gdm or lightdm-gtk-greeter provides. 
+# sublime text needs gtk2 which gdm or lightdm-gtk-greeter provides.
 # gdm is pretty, but slow, clunky, and doesn't work with dwm alone, need jwm or others, and wouldn't take dwm as default
-# dwm is compiled from 2000 lines of C code. Any customization requires mod of source code and recompilation. 
+# dwm is compiled from 2000 lines of C code. Any customization requires mod of source code and recompilation.
 # dwm is very fast, secure, effecient, and clean keyboard driven window manager
 # dmenu is a searchable menu system that works with dwm
 # make and gcc is needed for vbox guest additions
-sudo apt-get install lightdm-gtk-greeter git curl  
+sudo apt-get install lightdm-gtk-greeter git curl
 
 # autologin to lightdm
 #sudo nano /etc/lightdm/lightdm.conf
   #[SeatDefaults]
   #autologin-user=<YOUR USER>
   #autologin-user-timeout=0
-sudo sed -i '$ a\ [SeatDefaults] 
-  utologin-user=<YOUR USER> 
+sudo sed -i '$ a\ [SeatDefaults]
+  utologin-user=<YOUR USER>
   autologin-user-timeout=0' /etc/lightdm/lightdm.conf
 
 # git config
@@ -41,37 +41,37 @@ echo 'sudo mount -t vboxsf share ~/share' >> .bashrc
 
 # other stuff
 sudo apt-get install python-software-properties software-properties-common
-
-sudo apt-get install libpq-dev bundler firefox libxml2-dev openjdk-7-jdk
+sudo apt-get install libcurl4-gnutls-dev htop gnome-system-monitor
+sudo apt-get install libpq-dev bundler firefox libxml2-dev openjdk-7-jdk 
 bundle install
 bundle update
 
 
 # Get the latest R version
-sudo apt-key adv –keyserver keyserver.ubuntu.com –recv-keys E084DAB9
-sudo add-apt-repository ‘deb http://star-www.st-andrews.ac.uk/cran/bin/linux/ubuntu trusty/’
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+sudo add-apt-repository ‘deb http://cran.rstudio.com/bin/linux/ubuntu trusty/’
 sudo apt-get update
 sudo apt-get install r-base
 
 # Download and install RStudio
-wget http://download1.rstudio.org/rstudio-0.98.1091-amd64.deb
+wget http://download1.rstudio.org/rstudio-0.99.332-amd64.deb
 sudo dpkg -i *.deb
 rm *.deb
 # R packages are installed in Rstudio
 
 # install miniconda for scientific python2.79
-wget http://repo.continuum.io/miniconda/Miniconda3-3.7.0-Linux-x86_64.sh -O ~/miniconda.sh
+wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
-wget http://repo.continuum.io/miniconda/Miniconda-3.8.3-Linux-x86_64.sh
-bash Miniconda-3.8.3-Linux-x86_64.sh
+# wget http://repo.continuum.io/miniconda/Miniconda-3.8.3-Linux-x86_64.sh
+# bash Miniconda-3.8.3-Linux-x86_64.sh
 conda update conda
 
 conda create -n boa python=2
 source activate boa
 conda install numpy pandas spyder scikit-learn matplotlib statsmodels
-source deactivate 
+source deactivate
 
 # sublime text 3
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
