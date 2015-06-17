baf7d42c8e2eed1c8d0730465a4cd14cc364fc59
Victor Wang <victoongo@gmail.com>
2015-06-17 00:35:57 -0400

    setting up java and scala, update R to 3.2

--- tiny_VM_setup_pub.sh
+++ tiny_VM_setup_pub.sh
@@ -12,7 +12,6 @@ sudo apt-get install --no-install-recommends xorg lightdm dwm dmenu make gcc
 # dwm is compiled from 2000 lines of C code. Any customization requires mod of source code and recompilation.
 # dwm is very fast, secure, effecient, and clean keyboard driven window manager
 # dmenu is a searchable menu system that works with dwm
-# make and gcc is needed for vbox guest additions
 sudo apt-get install lightdm-gtk-greeter git curl
 
 # autologin to lightdm
@@ -31,8 +30,7 @@ sudo sed -i '$ a\ [SeatDefaults]
 # ssh-add ~/.ssh/id_rsa
 
 # vbox guestaddition
-sudo apt-get install -y dkms build-essential linus-headers-generic linus-headers-$(uname -r)
-make gcc --no-install-recommends
+sudo apt-get install -y dkms build-essential linus-headers-generic linus-headers-$(uname -r) --no-install-recommends
 sudo mkdir /media/cdrom
 sudo mount /dev/cdrom /media/cdrom
 sudo /media/cdrom/VBoxLinuxAdditions.run
@@ -42,10 +40,15 @@ echo 'sudo mount -t vboxsf share ~/share' >> .bashrc
 # other stuff
 sudo apt-get install python-software-properties software-properties-common
 sudo apt-get install libcurl4-gnutls-dev htop gnome-system-monitor
-sudo apt-get install libpq-dev bundler firefox libxml2-dev openjdk-7-jdk 
+sudo apt-get install libpq-dev bundler firefox libxml2-dev openjdk-7-jdk libgtk-3-dev libgtk2.0-dev
 bundle install
 bundle update
 
+# check Ubuntu and Kernel version
+lsb_release -a
+lsb_release -r
+uname -a
+uname -r
 
 # Get the latest R version
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
@@ -58,10 +61,25 @@ wget http://download1.rstudio.org/rstudio-0.99.332-amd64.deb
 sudo dpkg -i *.deb
 rm *.deb
 # R packages are installed in Rstudio
+# reconfig java home after installing openjdk to install rjava
+sudo R CMD javareconf
+# in R run 
+# install.packages('rJava')
+
+# update R and mv libraries
+mv ~/R/x86_64-pc-linux-gnu-library/3.1/* ~/R/x86_64-pc-linux-gnu-library/3.2
+# in R run 
+update.packages(checkBuilt=TRUE, ask=FALSE)
+# remove.packages("BiocInstaller")
+# source("http://bioconductor.org/biocLite.R")
+# biocLite()
+# then remove old R library folder
+rm -rf ~/R/x86_64-pc-linux-gnu-library/3.1
 
 # install miniconda for scientific python2.79
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
+# put this in ~/.profile
 export PATH="$HOME/miniconda/bin:$PATH"
 
 # wget http://repo.continuum.io/miniconda/Miniconda-3.8.3-Linux-x86_64.sh
@@ -70,8 +88,17 @@ conda update conda
 
 conda create -n boa python=2
 source activate boa
-conda install numpy pandas spyder scikit-learn matplotlib statsmodels
+conda install numpy pandas spyder scikit-learn matplotlib statsmodels 
+conda install pip num2words
 source deactivate
+
+# install bluej via deb/dpkg
+# drjava only need to be unpacked to run
+
+# install sbt
+echo "deb http://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-get update
+sudo apt-get install sbt
 
 # sublime text 3
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
